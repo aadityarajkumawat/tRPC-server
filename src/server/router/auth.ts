f98b54ab85aadd7ba1f907b3b9ff6dba8d0084b4
Aditya Raj Kumawat <arkumawat78@gmail.com>
2022-08-10 03:00:32 +0530

    simply register

--- src/server/router/auth.ts
+++ src/server/router/auth.ts
@@ -133,35 +133,35 @@ export const authRouter = router
             try {
                 const { req, user: userSession, db, sessionStore } = ctx
 
-                const { sessionBySessionId, destroySession, createSession } =
-                    sessionStore
+                // const { sessionBySessionId, destroySession, createSession } =
+                //     sessionStore
 
-                if (req.user) {
-                    // user loaded from access token
-                    // Authorization header is not supposed
-                    // to be found here.
-                }
+                // if (req.user) {
+                //     // user loaded from access token
+                //     // Authorization header is not supposed
+                //     // to be found here.
+                // }
 
-                if (userSession && userSession.payload) {
-                    // user session is loaded from the refresh token
-                    // not since the previous user is logging out
-                    // we revoke all their previous refresh tokens by
-                    // incrementing their token version
-                    const sessionId = userSession.payload.sessionId
+                // if (userSession && userSession.payload) {
+                //     // user session is loaded from the refresh token
+                //     // not since the previous user is logging out
+                //     // we revoke all their previous refresh tokens by
+                //     // incrementing their token version
+                //     const sessionId = userSession.payload.sessionId
 
-                    const session = await sessionBySessionId(sessionId)
-                    if (!session) throw new Error('Session not found, HERE')
+                //     const session = await sessionBySessionId(sessionId)
+                //     if (!session) throw new Error('Session not found, HERE')
 
-                    await destroySession(sessionId)
-                    req.setCookie(REFRESH_TOKEN, '', 0)
+                //     await destroySession(sessionId)
+                //     req.setCookie(REFRESH_TOKEN, '', 0)
 
-                    // this is where we invalidate the refresh token
-                    // by incrementing the tokenVersion
-                    await db.authToken.update({
-                        where: { userId: session.userId },
-                        data: { tokenVersion: { increment: 1 } },
-                    })
-                }
+                //     // this is where we invalidate the refresh token
+                //     // by incrementing the tokenVersion
+                //     await db.authToken.update({
+                //         where: { userId: session.userId },
+                //         data: { tokenVersion: { increment: 1 } },
+                //     })
+                // }
 
                 const { email, password } = input
 
@@ -173,31 +173,31 @@ export const authRouter = router
 
                 // if the user if getting created for the first time
                 // then we create a new authToken entry
-                const authToken = await db.authToken.create({
-                    data: {
-                        authTokenId: v4(),
-                        userId: registeredUser.userId,
-                    },
-                })
+                // const authToken = await db.authToken.create({
+                //     data: {
+                //         authTokenId: v4(),
+                //         userId: registeredUser.userId,
+                //     },
+                // })
 
-                const session = await createSession(
-                    registeredUser.userId,
-                    authToken.tokenVersion,
-                )
+                // const session = await createSession(
+                //     registeredUser.userId,
+                //     authToken.tokenVersion,
+                // )
 
-                const accessToken = signAccessToken({
-                    userId: registeredUser.userId,
-                    sessionId: session.sessionId,
-                })
+                // const accessToken = signAccessToken({
+                //     userId: registeredUser.userId,
+                //     sessionId: session.sessionId,
+                // })
 
-                const refreshToken = signRefreshToken({
-                    sessionId: session.sessionId,
-                    tokenVersion: authToken.tokenVersion,
-                })
+                // const refreshToken = signRefreshToken({
+                //     sessionId: session.sessionId,
+                //     tokenVersion: authToken.tokenVersion,
+                // })
 
-                req.setCookie(REFRESH_TOKEN, refreshToken, ONE_YEAR)
+                // req.setCookie(REFRESH_TOKEN, refreshToken, ONE_YEAR)
 
-                return { token: accessToken, error: null }
+                return { token: '<token>', error: null }
             } catch (error) {
                 if (error instanceof PrismaClientKnownRequestError) {
                     if (error.code === 'P2002') {
