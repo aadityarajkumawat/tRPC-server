6de545c6890b5aaaa01a28ea239f3bbeae14d6c8
Aditya Raj Kumawat <arkumawat78@gmail.com>
2022-08-09 21:02:18 +0530

    trying without global

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,12 +1,12 @@
 import { __is_prod__ } from '@constants'
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-    var db: PrismaClient | undefined
-}
+// declare global {
+//     var db: PrismaClient | undefined
+// }
 
-export const db = global.db || new PrismaClient()
+export const db = new PrismaClient()
 
-if (!__is_prod__) {
-    global.db = db
-}
+// if (!__is_prod__) {
+//     global.db = db
+// }
